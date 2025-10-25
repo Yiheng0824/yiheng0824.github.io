69149ce56894618ad73c1a6f0d7a395659285816
Yiheng0824 <zhang2968@wisc.edu>
2025-10-24 23:58:15 -0500

    deploy

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/phd-homepage", // 仓库名，必须加上
-  assetPrefix: "/phd-homepage/", // 静态资源路径前缀
   images: { unoptimized: true },
 };
 
